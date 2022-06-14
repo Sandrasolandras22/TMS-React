029db84c26637a0eadd071686b81ff031372fa35
Beata Shevando <beatashevando@gmail.com>
2022-06-14 21:17:34 +0300

    react_add

---
+++ react-beginner/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { isDataView } from "util/types";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render("div");
